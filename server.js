0cab6e3e7aa90816d8a81de0f6b4efdab6efd70f
Jamal Hasuev <metafra@mail.ru>
2021-08-01 02:45:58 +0300

    add some logic for server

--- server.js
+++ server.js
@@ -3,11 +3,71 @@ const express = require("express")
 const app = express()
 const server = require("http").createServer()
 const io = require("socket.io")(server)
+const ACTIONS = require("./src/socket/actions")
 
 const PORT = process.env.PORT || 3001
 
+function getClientRooms() {
+  const { rooms } = io.sockets.adapter
+
+  return Array.from(rooms.keys())
+}
+
+function shareRoomsInfo() {
+  io.emit(ACTIONS.SHARE_ROOM, {
+    rooms: getClientRooms()
+  })
+}
+
 io.on("connection", socket => {
-  console.log("Socket connected!");
+  shareRoomsInfo()
+
+  socket.on(ACTIONS.JOIN, config => {
+    const { room: roomID } = config
+    const { rooms: joinedRooms } = socket
+
+    if(Array.from(joinedRooms).includes(roomID)) {
+      return console.warn(`Alreay joined to ${roomID}`)
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || [])
+
+    clients.forEach(clientID => {
+      io.to(clientID).emit(ACTIONS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false,
+      })
+      socket.emit(ACTIONS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      })
+    })
+
+    socket.join(roomID)
+    shareRoomsInfo()
+  })
+
+  function leaveRoom() {
+    const {rooms} = socket
+
+    Array.from(rooms).forEach(roomID => {
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || [])
+
+      clients.forEach(clientID => {
+        io.to(clientID).emit(ACTIONS.REMOVE_PEER, {
+          peerID: socket.id
+        })
+        socket.emit(ACTIONS.REMOVE_PEER, {
+          peerID: clientID
+        })
+      })
+    })
+
+    shareRoomsInfo()
+  }
+
+  socket.on(ACTIONS.LEAVE, leaveRoom)
+  socket.on("disconnecting", leaveRoom)
 })
 
 server.listen(PORT, () => {
